7d0ecbc71a525fd447c66f4cdbe7a2af56fad3c3
lordypopdy-lab <deelordpopdy2@gmail.com>
2025-03-05 20:08:18 -0800

    safe

--- forexclient/src/App.jsx
+++ forexclient/src/App.jsx
@@ -15,10 +15,10 @@ import { Toaster } from 'react-hot-toast';
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-axios.defaults.baseURL = 'https://forexserver.vercel.app/';
+axios.defaults.baseURL = 'https://forexserver.vercel.app';
 axios.defaults.withCredentials = true;
 
-//https://forexserver.vercel.app/
+//https://forexserver.vercel.app
 
 function App() {
 
